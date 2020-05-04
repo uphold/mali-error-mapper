44b52c2ffaa5c65b1078d1549b0a3f975c510bbe
Tiago Leite <tiago.leite@uphold.com>
2020-05-04 09:35:47 +0100

    Add stringify to metadata objects

--- src/middlewares/error-mapper-middleware.js
+++ src/middlewares/error-mapper-middleware.js
@@ -4,6 +4,7 @@
  * Module dependencies.
  */
 
+const _ = require('lodash');
 const GrpcError = require('grpc-error');
 const fallbackErrorMapper = require('../mappers/fallback-error-mapper');
 const grpcErrorMapper = require('../mappers/grpc-error-mapper');
@@ -37,10 +38,18 @@ module.exports = function(mappers = []) {
         mapping = map(e);
       }
 
-      const metadata = {
+      let metadata = {
         code: mapping.body.code,
         ...mapping.metadata
       };
+
+      metadata = _.transform(metadata, (result, value, key) => {
+        if (_.isPlainObject(value)) {
+          result[key] = JSON.stringify(value);
+        } else {
+          result[key] = value;
+        }
+      });
 
       throw new GrpcError(mapping.body.message, mapping.status, metadata);
     }
